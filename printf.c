5f3a4b8fdfd88f2df1ac2b53d9ffe9220b4d80c2
Park Junsu <krpjs0508@gmail.com>
2022-03-22 23:13:07 +0900

    printf
    2022/03/07

---
+++ printf.c
@@ -0,0 +1,47 @@
+// %d 정수(int)
+// %f 실수(float, double)
+// %c 문자(character)
+// %s 문자열(string)
+
+
+
+//
+//#include <stdio.h>
+//int main() {
+//	printf("프로그래밍 수업 화이팅\n");
+//	printf("%d\n", 1234);
+//	printf("%d %d", 10, 20);
+//
+//	return 0;
+//}
+
+
+
+
+//#include <stdio.h>
+//int main() {
+//	printf("제 이름은 홍길동입니다.\n제 나이는 %d살입니다.\n제 전화번호는 %s입니다.", 20, "010-1234-5679");
+//}
+//
+
+
+
+//#include <stdio.h>
+//int main() {
+//	printf("%d * %d = %d\n", 4, 5, 4 * 5);
+//	printf("%d * %d = %d\n", 7, 9, 7 * 9);
+//}
+
+
+
+
+
+//#include <stdio.h>
+//
+//int main() {
+//	int num1 = 1;
+//	float num2 = 1;
+//	char c = 'a';
+//	printf("num1(int) : %d\nnum2(float) : %f\nc(char) : %c", num1, num2, c);
+// return 0;
+//}
